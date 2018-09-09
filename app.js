68d9445f86913b051dcbd0f86605f22349dae751
Bigmckissack <mckissack.kyle@gmail.com>
2018-09-08 20:55:20 -0400

    add express

--- app.js
+++ app.js
@@ -1,24 +1,71 @@
-var http = require('http');
 
+/*
+	Fundamental node code
+*/
+
+// var http = require('http');
 
 //create a server object:
 // http.createServer(function (req, res) {
 //   res.write('Hello World!'); //write a response to the client
 //   res.end(); //end the response
-// }).listen(8080); //the server object listens on port 8080
+// }).listen(1337); //the server object listens on port 1337
 
 
 
-// function to use instead of the one defined inline
-var routeHandler = function(req, res){
-	if(req.url === '/found'){
-		res.write('Found me!');
-		res.end();
-	}else{
-		res.write('Hello World!');
-		res.end();
-	}
-};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-//create a server object:
-http.createServer(routeHandler).listen(8080);
+
+/* 
+	Function to use instead of the one defined inline
+*/
+
+// var http = require('http');
+
+// var routeHandler = function(req, res){
+// 	if(req.url === '/found'){
+// 		res.write('Found me!');
+// 		res.end();
+// 	}else{
+// 		res.write('Hello World!');
+// 		res.end();
+// 	}
+// };
+
+// http.createServer(routeHandler).listen(1337);
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/*
+	There is a very good web routes framework module that we will use called Express
+	You need to make sure you have express installed with npm.
+	Readme should have you run 
+	>npm i
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/found', function(req, res) {
+  res.send('Found me');
+});
+
+app.all('*', function(req,res){
+	res.send('Else')
+})
+
+app.listen(1337, function() {
+  console.log('Example app listening on port 1337!');
+});
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
